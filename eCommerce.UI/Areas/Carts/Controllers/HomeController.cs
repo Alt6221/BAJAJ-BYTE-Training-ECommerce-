2ae970e09af4f2eb46b1350d0ffe250ef6dfa46c
Altaf Khan <100257576+Alt6221@users.noreply.github.com>
2024-02-04 22:56:03 +0530

    Add files via upload

---
+++ eCommerce.UI/Areas/Carts/Controllers/HomeController.cs
@@ -0,0 +1,104 @@
+﻿using eCommerce.DAL;
+using eCommerce.Models;
+using Microsoft.AspNetCore.Mvc;
+using Stripe;
+using Stripe.Checkout;
+using Microsoft.Extensions.Options;
+namespace eCommerce.UI.Areas.Carts.Controllers
+{
+    [Area("Carts")]
+    public class HomeController : Controller
+    {
+        //URL Carts/Home/MyCart
+        private readonly INewCartRepository _newCartRepository;
+        private readonly ICommonRepository<CartDetail> _cartDetailRepository;
+		private readonly StripeSettings _stripeSettings;
+		public string sessionId { get; set; }
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public HomeController(INewCartRepository newCartRepository, ICommonRepository<CartDetail> cartDetailRepository,IOptions<StripeSettings> stripeSettings)
+        {
+            _newCartRepository = newCartRepository;
+            _cartDetailRepository = cartDetailRepository;
+			_stripeSettings = stripeSettings.Value;
+		}
+
+        public async Task<IActionResult> MyCart()
+        {
+            var myCartItems = await _newCartRepository.GetCartDetails((int)HttpContext.Session.GetInt32("CartId"));
+            return View(myCartItems);
+        }
+        public async Task<IActionResult> AddToCart(int id)
+        {
+            //Please check do you have customerid in session. If not, redirect the user to the login page.
+            //After successful login, store the customer Id into session variable and comeback to AddToCart action method.
+            HttpContext.Session.SetInt32("CustomerId", 1);
+            if(HttpContext.Session.GetInt32("CartId")==null)
+            {
+                int cartId = await _newCartRepository.GenerateNewCart((int)HttpContext.Session.GetInt32("CustomerId"));
+                HttpContext.Session.SetInt32("CartId",cartId);
+            }
+            
+            await _cartDetailRepository.InsertAsync(new()
+            {
+                CartId = (int)HttpContext.Session.GetInt32("CartId"),
+                ProductId = id
+            });
+            return RedirectToAction("MyCart");
+        }
+		public async Task<IActionResult> RemoveFromCart(int Id)
+		{
+            await _cartDetailRepository.DeleteAsync(Id) ;
+			return RedirectToAction("MyCart");
+		}
+		public async Task<IActionResult> CreateCheckoutSessionAsync(string amount)
+		{
+			var myCartItems = await _newCartRepository.GetCartDetails((int)HttpContext.Session.GetInt32("CartId"));
+
+			var currency = "inr"; // Currency code
+
+			StripeConfiguration.ApiKey = _stripeSettings.SecretKey;
+			var options = new SessionCreateOptions
+			{
+				LineItems = myCartItems.Select(item => new SessionLineItemOptions
+				{
+					PriceData = new SessionLineItemPriceDataOptions
+					{
+						UnitAmount = (long)(item.TotalAmount * 100),
+						Currency = currency, // Change this to your currency
+						ProductData = new SessionLineItemPriceDataProductDataOptions
+						{
+							Name = item.ProductName,
+						},
+					},
+					Quantity = 1,
+				}).ToList(),
+				Mode = "payment",
+				SuccessUrl = Url.Action("OrderSuccess", "Home", new { area = "Carts" }, HttpContext.Request.Scheme),
+				CancelUrl = Url.Action("OrderCancelled", "Home", new { area = "Carts" }, HttpContext.Request.Scheme),
+			};
+			var service = new SessionService();
+			var session = service.Create(options);
+			sessionId = session.Id;
+			return Redirect(session.Url);
+		}
+
+		public async Task<IActionResult> success()
+		{
+			// Handle successful payment, e.g., update order status
+			return View("Index");
+		}
+
+		public IActionResult cancel()
+		{
+			// Handle cancelled payment, e.g., show a message to the user
+			return View("Index");
+		}
+
+
+	}
+}
---
+++ eCommerce.UI/Areas/Products/Controllers/HomeController.cs
@@ -0,0 +1,81 @@
+﻿using eCommerce.DAL;
+using eCommerce.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace eCommerce.UI.Areas.Products.Controllers
+{
+    [Area("Products")]
+    public class HomeController : Controller
+    {
+        private readonly ICommonRepository<Product> _productsrepo;
+        private readonly IMemoryCache _productsCache;
+
+
+
+        public HomeController(ICommonRepository<Product> productsrepo, IMemoryCache productsCache)
+        {
+            _productsrepo = productsrepo;
+            _productsCache = productsCache;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            ViewBag.PageTitle = "eCommerce Products List!";
+            ViewBag.PageSubTitle = "Find all the products under single roof";
+            if(_productsCache.TryGetValue("eCommerceProducts",out List<Product>products)) //the data will be retrieved from cache
+                {
+                return View(products);
+            }
+            var allProducts=await _productsrepo.GetAllAsync(); // if its not present in cache, it connects with the database and fetched the data, shows it in UI.
+            var cacheEntryOptions = new MemoryCacheEntryOptions().SetAbsoluteExpiration(new TimeSpan(0, 10, 0)).SetSlidingExpiration(new TimeSpan(0,1,0)); //data will remain for 1min in cache
+            _productsCache.Set("eCommerceProductsCache", allProducts, cacheEntryOptions);
+            return View(allProducts);
+        }
+        public async Task<IActionResult> Details(int id)
+        {
+            ViewBag.PageTitle = "Details of";
+            var product=await _productsrepo.GetDetailAsync(id);
+            double discountedAmount = product.UnitPrice- ((product.UnitPrice * product.Discount) / 100);
+            ViewBag.Discount=discountedAmount;
+            return View(product);
+        }
+        public async Task<ActionResult> GetParticularCategoryProducts(int id)
+        {
+            ViewBag.PageTitle = "eCommerce Products List";
+            ViewBag.PageSubTitle = "Find all products under single roof using Category ID";
+            var products = await _productsrepo.GetAllAsync();
+            List<Product> categoryProducts = new List<Product>();
+            foreach (var product in products)
+            {
+                if (product.CategoryId == id)
+                {
+                    categoryProducts.Add(product);
+                }
+
+            }
+            return View(categoryProducts);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _productsrepo.InsertAsync(product);
+                if (result > 0)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return View();
+                }
+            }
+            return View();
+        }
+    }
+}
