f11f9c20ab4ca3af0015ea8de2de107e0e21c7d7
Altaf Khan <100257576+Alt6221@users.noreply.github.com>
2024-02-04 22:35:32 +0530

    Add files via upload

---
+++ eCommerce.Models/Cart.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace eCommerce.Models
+{
+    public class Cart
+    {
+        public int CartId { get; set; }
+        public DateTime CartDate { get; set; } = DateTime.Now;
+        public int CustomerId { get; set; }
+
+        //Navigation property
+        public virtual Customer Customer { get; set; } //connected to one customer'.
+
+        public ICollection<CartDetail>? CartDetails { get; set; }
+        public virtual Invoice InvoiceId { get; set; }
+    }
+}
---
+++ eCommerce.Models/Customer.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace eCommerce.Models
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+
+        [Required(ErrorMessage="Customer Name is a required field!")]
+        [MaxLength(100,ErrorMessage ="Customer name cannot exceed 100 characters!")]
+        public string ContactName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Customer Address is a required field!")]
+        [MaxLength(500, ErrorMessage = "Customer Address cannot exceed 500 characters!")]
+        public string Address { get; set; }= string.Empty;
+
+        [Required(ErrorMessage = "City is a required field!")]
+        [MaxLength(100, ErrorMessage = "City name cannot exceed 100 characters!")]
+        public string City { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Email is a required field!")]
+        [MaxLength(100, ErrorMessage = "Email Id cannot exceed 100 characters!")]
+        [EmailAddress(ErrorMessage ="Please enter valid email address")]
+        public string Email { get; set; }= string.Empty;
+        public virtual ICollection<Cart>? Carts { get; set; } //connected to many carts
+
+        [Required(ErrorMessage = "Phone number is a required field!")]
+        [MaxLength(20, ErrorMessage = "Phone Number cannot exceed 20 characters!")]
+        public string Phone { get; set; } = string.Empty;
+    }
+}
